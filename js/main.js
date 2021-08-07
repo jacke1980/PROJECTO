106fd890312202f4f8833d04f61206d823eb3d8f
Jackie270 <jgonza270@icloud.com>
2021-08-07 10:04:28 -0400

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const collapsibles = document.querySelectorAll(".collapsible");
+const item = document.querySelector(".nav__toggler");
 collapsibles.forEach((item)=>
 item.addEventListener("click", function(){
     this.classList.toggle("collapsible--expanded");
